b7c90146a6676f07bfacf05728bab5f033c62b41
Hanibans <hannahconcillado@gmail.com>
2025-10-02 21:43:17 +0200

    Torsdagsopgaver

---
+++ Task2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        Cafe myCafe = new Cafe();
+
+        myCafe.loadMenuData();
+
+        System.out.println("Coffee Menu");
+        for (String coffee : myCafe.getCoffeeMenu()){
+            System.out.println("- " + coffee);
+        }
+    }
+}
